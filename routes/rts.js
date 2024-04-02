424b25afc49c1593210f0c88e6b34527b643f519
Tolulopoe <treasureddammie@gmail.com>
2024-04-02 12:14:20 +0100

    ROUTES

--- routes/rts.js
+++ routes/rts.js
@@ -5,7 +5,7 @@ const {verifyAuth} = require('../middleware/auth')
 const {home, about,details} = require('../controllers/users');
 
 const {signup} = require('../controllers/signup');
-const {login} = require('../controllers/graceLogin');
+const {Login} = require('../controllers/userLogin');
 // const {forgotPassword} = require('../controllers/forgotPasSword');
 const {forgotPass} = require('../controllers/forgotPass');
 const {resetPassword} = require('../controllers/RESET');
@@ -16,7 +16,7 @@ const {verifyOtp} = require('../controllers/verifyOtp');
 // const {insertCus} = require('../controllers/customerControl')
 
 router.get('/',home)
-router.get('/login',login)// local database
+router.get('/login',Login)// local database
 // router.get('/register',insertCus)//clever cloud access
 router.post('/signup',signup)// local database 
 // router.post('/forgotPassword',forgotPassword)// local database
